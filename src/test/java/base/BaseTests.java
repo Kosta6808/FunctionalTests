6f623b8633ca5f68791b973e8851712c1267b193
Kosta6808 <kosta6808@mail.ru>
2025-10-23 00:18:37 +0300

    Adding selenium test

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,42 @@
+// BaseTests.java
+package base;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import java.time.Duration;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
+import java.io.File;
+import java.io.IOException;
+import com.google.common.io.Files;
+
+
+public class BaseTests {
+    protected WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://iytnet.com/index.php?option=com_comprofiler&task=login&Itemid=198");
+    }
+
+    @AfterMethod
+    public void teardown(ITestResult result){
+        if(ITestResult.FAILURE == result.getStatus()) {
+            var camera = (TakesScreenshot) driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot,
+                        new File("src/main/resources/screenshots/" +
+                                result.getName() + ".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        driver.quit();
+    }
+}
---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,18 @@
+// LoginTest.java
+package tests;
+import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import static org.testng.Assert.*;
+
+public class LoginTest extends BaseTests {
+    @Test
+    public void verifySuccessfulLogin(){
+        LoginPage loginPage = new LoginPage(driver);
+        assertEquals(loginPage.login(
+                "102945",
+                "jtuxbzwh"
+                ).getTitle(),
+                "https://iytnet.com/");
+    }
+}
