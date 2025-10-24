1422d47b5689a1b10767f60932a6a5337083e179
Kosta6808 <kosta6808@mail.ru>
2025-10-24 12:55:02 +0300

    Add new date: login, password, adress(Jenkins)

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -2,6 +2,7 @@
 package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import java.time.Duration;
@@ -21,7 +22,7 @@ public class BaseTests {
                 "src/main/resources/chromedriver");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://iytnet.com/index.php?option=com_comprofiler&task=login&Itemid=198");
+        driver.get("http://localhost:8080/");
     }
 
     @AfterMethod
